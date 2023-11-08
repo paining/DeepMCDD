1f16dbb0f3a8c8c9f2b72fcc0dafcbf07a5a4527
ysh_alcyone <yearnsh@pleiony.com>
2023-11-09 06:36:48 +0900

    merging

--- train_vae.py
+++ train_vae.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, Dataset, TensorDataset
-from torch.utils.tensorboard.writer import SummaryWriter
+from torch.utils.tensorboard import SummaryWriter
 import torch.nn.functional as TF
 from torch.utils.data import DataLoader
 import torchvision.transforms as TVT
@@ -78,6 +78,10 @@ def main():
     )
     if args.ckpt is not None:
         model.load_state_dict(torch.load(args.ckpt, map_location="cpu"))
+        logger.info(
+            "Pretrained Model is loaded. (%s)",
+            args.ckpt if len(args.ckpt) < 60 else '...' + args.ckpt[-57:]
+        )
     model.to(device)
     data = torch.zeros((1,1,16,16), device=device)
     model(data)
@@ -103,6 +107,15 @@ def main():
             device,
             args.save_path
         )
+        # train_patch(
+        #     model,
+        #     train_loader,
+        #     test_loader,
+        #     args.num_epochs,
+        #     args.learning_rate,
+        #     device,
+        #     args.save_path
+        # )
     else:
         test_loader = get_dataloader(os.path.join(args.data_path, "test"), args.gt_path)
         log_dir = os.path.join(
@@ -229,7 +242,7 @@ def train(
         train_loss = []
         train_re = []
         train_kld = []
-        grad_max = 0
+        max_grad = 0
         model.train()
         for x, gt, y, filename in tqdm(train_loader, ncols=79, desc="Train", leave=False):
             with torch.no_grad():
@@ -313,6 +326,141 @@ def train(
                 x_, mu, logvar = model(x_patch)
 
                 re = model.RE(x_, x_patch)
+                kld = beta * model.KLD(mu, logvar)
+                loss = re + kld
+                valid_loss.append(loss.item())
+                valid_re.append(re.item())
+                valid_kld.append(kld.item())
+
+        valid_loss = torch.mean(torch.tensor(valid_loss)).item()
+        valid_re = torch.mean(torch.tensor(valid_re)).item()
+        valid_kld = torch.mean(torch.tensor(valid_kld)).item()
+        logger.info(f"Epoch {epoch:3d} : Valid : Loss = {valid_loss:10f}(RE:{valid_re:10f}, KLD:{valid_kld:10f})")
+
+        writer.add_scalar("loss/Train:RE", train_re, epoch)
+        writer.add_scalar("loss/Train:KLD", train_kld, epoch)
+        writer.add_scalar("loss/Train", train_loss, epoch)
+        writer.add_scalar("loss/Valid:RE", valid_re, epoch)
+        writer.add_scalar("loss/Valid:KLD", valid_kld, epoch)
+        writer.add_scalar("loss/Valid", valid_loss, epoch)
+        writer.add_scalar("Param/beta", beta, epoch)
+        writer.add_scalar("Param/max_grad", max_grad, epoch)
+
+        if valid_loss < best_loss:
+            best_loss = valid_loss
+            torch.save(model.state_dict(), os.path.join(log_dir, "models", "best.pt"))
+        if (epoch+1) % 10 == 0:
+            torch.save(model.state_dict(), os.path.join(log_dir, "models", f"{epoch}.pt"))
+        torch.save(model.state_dict(), os.path.join(log_dir, "models", "latest.pt"))
+
+
+def train_patch(
+        model:VAE,
+        train_loader:DataLoader,
+        test_loader:DataLoader,
+        epochs:int,
+        learing_rate:float,
+        device:torch.device,
+        log_dir:str
+        ):
+    os.makedirs(os.path.join(log_dir, "tensorboard"), exist_ok=True)
+    writer = SummaryWriter(os.path.join(log_dir, "tensorboard"))
+    optimizer = optim.Adam(model.parameters(), lr=learing_rate)
+    patch_size = 16
+    stride = 8
+    best_loss = 1e10
+    mini_batch = 1024
+
+    beta_arr = np.concatenate([
+        # np.zeros((epochs//4,)),
+        0.001 * np.ones((epochs//4,)),
+        np.linspace(0.001, 1, 2*(epochs//4), endpoint=True),
+        np.ones((epochs - (3*(epochs//4)),))
+    ])
+    fig, ax = plt.subplots()
+    ax.plot(beta_arr)
+    ax.set_title("beta-annealing")
+    fig.tight_layout()
+    fig.savefig(os.path.join(log_dir, "beta.png"))
+    plt.close(fig)
+
+    # Make Patch-dataset
+    patch_dataset = []
+    for x, gt, y, filename in tqdm(train_loader, ncols=79, desc="Extracting", leave=False):
+        with torch.no_grad():
+            h1 = int((x.shape[-2] - patch_size)/stride + 1)
+            w1 = int((x.shape[-1] - patch_size)/stride + 1)
+            x_patch = TF.unfold(x.mean(dim=1, keepdim=True), kernel_size=patch_size, stride=patch_size//2)
+            x_patch = rearrange(
+                x_patch,
+                "b (h2 w2) (h1 w1) -> (b h1 w1) 1 h2 w2",
+                h2=patch_size,
+                w2=patch_size,
+                h1=h1,
+                w1=w1
+            )
+            patch_dataset.append(x_patch)
+    patch_dataset = torch.cat(patch_dataset, dim=0)
+    patch_dataset = TensorDataset(patch_dataset)
+    dataloader = DataLoader(patch_dataset, mini_batch, shuffle=True, num_workers=4)
+
+    for epoch in trange(epochs, desc="Epochs", ncols=79):
+        # beta = min(1, max(0.001, 20.*(epoch-10)/epochs))
+        beta:float = beta_arr[epoch].item()
+        train_loss = []
+        train_re = []
+        train_kld = []
+        max_grad = 0
+        model.train()
+        for x in tqdm(dataloader, ncols=79, desc="Train", leave=False):
+            x = x[0].to(device)
+            optimizer.zero_grad()
+            x_, mu, logvar = model(x)
+
+            re = model.RE(x_, x)
+            kld = beta * model.KLD(mu, logvar)
+            loss = re + kld
+            train_loss.append(loss.item())
+            train_re.append(re.item())
+            train_kld.append(kld.item())
+
+            loss.backward()
+            for name, param in model.named_parameters():
+                grad_ = torch.max(param.grad).item()
+                if grad_ > max_grad:
+                    max_grad = grad_
+            optimizer.step()
+
+        train_loss = torch.mean(torch.tensor(train_loss)).item()
+        train_re = torch.mean(torch.tensor(train_re)).item()
+        train_kld = torch.mean(torch.tensor(train_kld)).item()
+        logger.info(f"Epoch {epoch:3d} : Train : Loss = {train_loss:10f}(RE:{train_re:10f}, KLD:{train_kld:10f})")
+
+        valid_loss = []
+        valid_re = []
+        valid_kld = []
+        model.eval()
+        
+        if epoch % 5 == 0:
+            visualize(model, test_loader, device, os.path.join(log_dir, "tmp", f"{epoch:03d}"))
+        for x, gt, y, filename in tqdm(test_loader, ncols=79, desc="Test", leave=False):
+            with torch.no_grad():
+                x = x.to(device)
+                h1 = int((x.shape[-2] - patch_size)/stride + 1)
+                w1 = int((x.shape[-1] - patch_size)/stride + 1)
+                x_patch = TF.unfold(x.mean(dim=1, keepdim=True), kernel_size=patch_size, stride=patch_size//2)
+                x_patch = rearrange(
+                    x_patch,
+                    "b (h2 w2) (h1 w1) -> (b h1 w1) 1 h2 w2",
+                    h2=patch_size,
+                    w2=patch_size,
+                    h1=h1,
+                    w1=w1
+                )
+
+                x_, mu, logvar = model(x_patch)
+
+                re = model.RE(x_, x_patch)
                 kld = model.KLD(mu, logvar)
                 loss = re + beta * kld
                 valid_loss.append(loss.item())
@@ -333,14 +481,14 @@ def train(
         writer.add_scalar("loss/Valid:beta*KLD", beta * valid_kld, epoch)
         writer.add_scalar("loss/Valid", valid_loss, epoch)
         writer.add_scalar("Param/beta", beta, epoch)
-        writer.add_scalar("Param/max_grad", grad_max, epoch)
+        writer.add_scalar("Param/max_grad", max_grad, epoch)
 
         if valid_loss < best_loss:
             best_loss = valid_loss
             torch.save(model.state_dict(), os.path.join(log_dir, "models", "best.pt"))
         if (epoch+1) % 10 == 0:
             torch.save(model.state_dict(), os.path.join(log_dir, "models", f"{epoch}.pt"))
-        torch.save(model.state_dict(), os.path.join(log_dir, "models", f"latest.pt"))
+        torch.save(model.state_dict(), os.path.join(log_dir, "models", "latest.pt"))
 
 def visualize(model, test_loader, device, log_dir):
     patch_size = 16
